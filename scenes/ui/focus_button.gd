90e0aac0eeb90d941aeacd9085c8e207cbd81195
Ancocodet <ancoplays@gmail.com>
2024-09-08 14:03:43 +0200

    add main menu

---
+++ scenes/ui/focus_button.gd
@@ -0,0 +1,12 @@
+extends Button
+
+func _ready():
+	if self.visible:
+		focus_button()
+
+func _on_visibility_changed() -> void:
+	focus_button()
+
+func focus_button():
+	self.grab_focus()
+	
---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_new_game_button_down() -> void:
+	pass # Replace with function body.
