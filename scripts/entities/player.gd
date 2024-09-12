00f20aff3619186210562c1cf38ea2c58884c6f8
Ancocodet <ancoplays@gmail.com>
2024-09-12 20:28:36 +0200

    Add back walking animation

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -21,6 +21,13 @@ func _physics_process(_delta) -> void:
 	if dashing:
 		return
 	
+	if direction.y < 0:
+		$sprite.play("walking_back")
+	elif direction.y > 0 or direction.x != 0:
+		$sprite.play("walking_front")
+	else:
+		$sprite.play("idle")
+		
 	if Input.is_action_pressed("move_dash") && dash_charge > 0:
 		dash(direction)
 	else:
@@ -29,9 +36,6 @@ func _physics_process(_delta) -> void:
 	if is_moving():
 		if dash_charge < 1.0:
 			dash_charge += 0.01
-		$sprite.play("walking")
-	else:
-		$sprite.play("idle")
 	
 	move_and_slide()
 
