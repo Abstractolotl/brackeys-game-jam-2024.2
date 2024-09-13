0705363ba249c1989845745aa3cdfb7f49fff8a0
Ancocodet <ancoplays@gmail.com>
2024-09-13 17:54:52 +0200

    Add options screen and credits

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -4,3 +4,23 @@ extends Control
 
 func _on_new_game_button_down() -> void:
 	get_tree().change_scene_to_file(game_scene.resource_path)
+
+
+func _on_options_button_down() -> void:
+	$Menu/TextureRect/HBoxContainer/Default.visible = false
+	$Menu/TextureRect/HBoxContainer/Options.visible = true
+
+func _on_back_button_down() -> void:
+	$Menu/TextureRect/HBoxContainer/Options.visible = false
+	$Menu/TextureRect/HBoxContainer/Default.visible = true
+
+
+func _on_credits_button_down() -> void:
+	$Menu.visible = false
+	$Credits.visible = true
+	
+	$AnimationPlayer.play("credits")
+	await $AnimationPlayer.animation_finished
+	
+	$Credits.visible = false
+	$Menu.visible = true
