21768dae8c263a4fe5f667296bd70c9a03599f44
Ancocodet <ancoplays@gmail.com>
2024-09-14 22:20:30 +0200

    Make options work

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -19,9 +19,9 @@ func _on_options_button_up() -> void:
 	AudioManager.play_sound(load("res://assets/audio/menu/menu_click_end.mp3"), "Effects")
 	$AnimationPlayer.play("show_options")
 
+
 func _on_credits_button_down() -> void:
 	AudioManager.play_sound(load("res://assets/audio/menu/menu_click_start.mp3"), "Effects")
-	$AnimationPlayer.play("back_to_start")
 	$Menu.visible = false
 	$Credits.visible = true
 	
@@ -34,3 +34,44 @@ func _on_credits_button_down() -> void:
 
 func _on_credits_button_up() -> void:
 	AudioManager.play_sound(load("res://assets/audio/menu/menu_click_end.mp3"), "Effects")
+
+
+func _on_back_button_down() -> void:
+	AudioManager.play_sound(load("res://assets/audio/menu/menu_click_start.mp3"), "Effects")
+
+
+func _on_back_button_up() -> void:
+	AudioManager.play_sound(load("res://assets/audio/menu/menu_click_end.mp3"), "Effects")
+	$AnimationPlayer.play("back_to_start")
+
+
+func _on_master_slider_value_changed(value: float) -> void:
+	if value <= -13:
+		AudioManager.set_bus_mute("Master", true)
+	else:
+		AudioManager.set_bus_mute("Master", false)
+		AudioManager.set_bus_volume("Master", value)
+
+
+func _on_effects_slider_value_changed(value: float) -> void:
+	if value <= -13:
+		AudioManager.set_bus_mute("Effects", true)
+	else:
+		AudioManager.set_bus_mute("Effects", false)
+		AudioManager.set_bus_volume("Effects", value)
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	if value <= -13:
+		AudioManager.set_bus_mute("Music", true)
+	else:
+		AudioManager.set_bus_mute("Music", false)
+		AudioManager.set_bus_volume("Music", value)
+
+
+func _on_enemies_slider_value_changed(value: float) -> void:
+	if value <= -13:
+		AudioManager.set_bus_mute("Enemies", true)
+	else:
+		AudioManager.set_bus_mute("Enemies", false)
+		AudioManager.set_bus_volume("Enemies", value)
