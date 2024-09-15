853dadc0144c3fdaaa63002622417a05d2b0a6de
Kevin Fischer <KevinFischer2010@gmail.com>
2024-09-15 11:47:27 +0200

    performance

--- scripts/entities/bullet.gd
+++ scripts/entities/bullet.gd
@@ -61,6 +61,9 @@ func _physics_process(delta: float) -> void:
 	rotation = (next_position - position).angle()
 	position = next_position
 
+	if flightTime > 10:
+		queue_free()
+
 func _quadratic_bezier(p0: Vector2, p1: Vector2, p2: Vector2, t: float) -> Vector2:
 	var q0: Vector2 = p0.lerp(p1, t)
 	var q1: Vector2 = p1.lerp(p2, t)
