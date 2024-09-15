7ed4dd0ac2e87d9d0009fa23675dca5ed8e06d1d
Kevin Fischer <KevinFischer2010@gmail.com>
2024-09-15 10:28:14 +0200

    invincible frames

--- scripts/components/health.gd
+++ scripts/components/health.gd
@@ -4,12 +4,22 @@ class_name HealthComponent
 @export var health: float = 10
 @export var max_health: float = 10.0
 
+@export var invincibility_time = 0.0
+
 signal health_changed(amount: float, new_health: float)
+
+var invincible_timer = 0.0
 
 func _ready() -> void:
 	health = max_health
 
+func _process(delta: float) -> void:
+	invincible_timer -= delta
+
 func hit(amount: float):
+	if invincible_timer > 0:
+		return
+	invincible_timer = invincibility_time
 	if not get_parent() is Player:
 		get_tree().get_current_scene().hud.show_damage_number(get_parent().global_position, amount)
 		
